bbba2f3b30e68a641f084ac801067673ce01c63b
ardipta <ashiqur35-2149@diu.edu.bd>
2020-05-10 20:28:14 +0600

    Featured List Added

--- firstproject/pages/views.py
+++ firstproject/pages/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
@@ -23,3 +24,24 @@ def about(request):
         'seller_of_the_month': seller_of_the_month
     }
     return render(request, 'pages/about.html', context)
+
+
+def featured(request):
+    featured_list = Listing.objects.filter(is_published=True)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(featured_list, 3)
+
+    try:
+        featured_list = paginator.page(page)
+
+    except PageNotAnInteger:
+        # fall back to first page
+        featured_list = paginator.page(1)
+    except EmptyPage:
+        # fall back to last page
+        featured_list = paginator.page(paginator.num_pages)
+    context = {
+        'featured_list': featured_list
+    }
+
+    return render(request, 'pages/featured.html', context)
